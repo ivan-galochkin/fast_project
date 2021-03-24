10676d2ad4a3f859ad76b6dc954a754586d39b41
IvanG <galochkinia@ugrafmsh.ru>
2021-03-24 20:00:47 +0500

    init

--- users_api/users_api.py
+++ users_api/users_api.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
 import fastapi
-from item_models import UserItem
+from models import UserItem
 from db_session import create_session, global_init
-from db_models import User
+from schemas import User
+from fastapi.middleware.cors import CORSMiddleware
 import sqlalchemy as sa
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8081",
+    "https://localhost:8081",
+    "localhost:8081"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/users")
